b620d2eace62d9f2dc183ac073c13dc1edb65bb3
Hoang Doan <hoang.doan@rocketmail.com>
2022-09-25 11:40:05 +0200

    hidden aws

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { saveAs } from 'file-saver';
 
 function App() {
+
+  const [url, setUrl] = useState('');
+  const [loading, setLoading] = useState('');
+
+  const handleSubmit = async (event, teamUrl) => {
+    event.preventDefault();
+    setLoading('Načítání ...')
+
+    const response = await axios.get(process.env.REACT_APP_AWS_TOKEN, {
+      params: {
+        teamUrl: teamUrl
+      }
+    })
+
+    const bytes = new TextEncoder().encode(response.data);
+    const blob = new Blob([bytes], { type: "text/calendar;charset=utf-8" });
+    saveAs(blob, "psmf.ics");
+    setLoading(" ");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <form style={{ margin: '20px' }} onSubmit={event => handleSubmit(event, url)}>
+      <h1>Gener&aacute;tor kalend&aacute;ře PSMF (iCalendar)</h1>
+      <ol>
+        <li>Vložte pros&iacute;m odkaz na PSMF str&aacute;nku va&scaron;eho t&yacute;mu. Např&iacute;klad <em><a href="https://www.psmf.cz/souteze/2022-hanspaulska-liga-podzim/8-c/tymy/kosticky/">https://www.psmf.cz/souteze/2022-hanspaulska-liga-podzim/8-c/tymy/kosticky/</a></em></li>
+        <li>Vygenerujte a st&aacute;hněte si kalend&aacute;řov&yacute; soubor</li>
+        <li>Kalend&aacute;řov&yacute; soubor (.ics) importujte do sv&eacute;ho kalend&aacute;ře.</li>
+      </ol>
+      <p><br />
+
+      <label>Odkaz na PSMF stránku týmu</label>
+      
+      <textarea cols="30" rows="2" style={{ width: '100%', height: '38px', marginTop: '5px' }} value={url}
+        onChange={(e) => setUrl(e.target.value)}></textarea><br /><br /><button type="submit" style={{height: '38px' }}>Generovat soubor</button></p>
+        <p>{loading}</p>
+    </form>
+
   );
 }
 
