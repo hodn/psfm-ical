84d3df17e3a584dbf484e6784cc2be294f148ab3
Hoang Doan <47562582+hodn@users.noreply.github.com>
2024-08-20 10:44:07 +0100

    Mantainance note

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { saveAs } from 'file-saver';
-import { Button, Collapse, Card, Text, Grid, Textarea, Spacer } from '@geist-ui/core'
+import { Note, Button, Collapse, Card, Text, Grid, Textarea, Spacer } from '@geist-ui/core'
 
 
 function App() {
